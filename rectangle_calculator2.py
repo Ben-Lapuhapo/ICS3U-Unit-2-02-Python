f4dcfc4064eec0c4551e19fc7f2fac54c7872621
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-16 17:26:20 +0000

    Fixed operations and codes

--- rectangle_calculator2.py
+++ rectangle_calculator2.py
@@ -5,19 +5,15 @@
 # This program calculates area and perimeter of a rectangle that the user gave
 
 
-
-
 def main():
     #input
     print("")
     length = int(input("Please enter the rectangle's length: "))
     print("")
     width = int(input("Please enter the rectangle's width: "))
-    
     #process
     area=length*width
     perimeter=2*(length+width)
-    
     #output
     print("")
     print("Area is {}mm^2".format(area))
